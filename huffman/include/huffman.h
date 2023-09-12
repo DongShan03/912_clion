bdcdd2710bf8ea1a1b882dbdd16e18f21148f1b8
DongShan03 <1135972990@qq.com>
2023-09-12 19:43:29 +0800

    basic structure of huffman

---
+++ huffman/include/huffman.h
@@ -0,0 +1,13 @@
+//
+// Created by huang on 2023/9/12.
+//
+
+#ifndef INC_912HUFFMAN_HUFFMAN_H
+#define INC_912HUFFMAN_HUFFMAN_H
+
+
+
+
+#include "huffman_implementation.h"
+
+#endif //INC_912HUFFMAN_HUFFMAN_H
---
+++ huffman/src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by huang on 2023/9/12.
+//
+
+
+#include "../include/huffman.h"
+
+
+int main() {
+    return 0;
+}
